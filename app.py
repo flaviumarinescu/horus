59734ff6188d8da55a273688f16366f69e2e9b9e
flaviu <flaviumarinescu@gmail.com>
2021-05-07 23:47:15 +0300

    running condition added

--- app.py
+++ app.py
@@ -217,9 +217,10 @@ if __name__ == "__main__":
 
     data = stock_data(yf_params, strategy_params, level_params)
 
-    plots = {}
-    ax = fplt.create_plot(yf_params["tickers"], rows=1, yscale="liniar")
+    if data:
+        plots = {}
+        ax = fplt.create_plot(yf_params["tickers"], rows=1, yscale="liniar")
 
-    update_plot(data["df"], data["levels"], contexts=strategy_params["contexts"])
-    fplt.timer_callback(update_plot, 30)
-    fplt.show()
+        update_plot(data["df"], data["levels"], contexts=strategy_params["contexts"])
+        fplt.timer_callback(update_plot, 30)
+        fplt.show()
